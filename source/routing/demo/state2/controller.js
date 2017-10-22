ce4529df47e93e47860fb34a81448ff91292acf7
Mostafa Shouman <shouman119@gmail.com>
2017-10-22 16:23:26 +0200

    create state2/controller.js

---
+++ source/routing/demo/state2/controller.js
@@ -0,0 +1,8 @@
+(function () {
+    Router.Controller(function ($scope) {
+        $scope.model = {};
+        $scope.model.name = "Ali";
+        $scope.model.age = 25;
+        $scope.salary = 1000;
+    });
+}())
