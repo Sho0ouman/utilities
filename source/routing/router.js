ae716ca2f9c8f89dff7041f24629d41feefc4903
Mostafa Shouman <shouman119@gmail.com>
2017-10-22 16:16:31 +0200

    Adding:events

--- source/routing/router.js
+++ source/routing/router.js
@@ -1,7 +1,14 @@
 var Router = (function () {
     var container,
         currState,
+        preState,
+        currController,
         routers;
+
+    var events = {
+        onPreStateChanged: function () {},
+        onStateChanged: function () {}
+    };
 
     (function init() {
         routers = [];
@@ -10,145 +17,196 @@ var Router = (function () {
 
     function register(r) {
         routers.push(r);
+        if (r.default)
+            go(r.name);
+        listenToUrlChange();
     }
 
-    function getRouter(name) {
+    function listenToUrlChange() {
+        setInterval(function () {
+            var url = window.location.hash;
+            url = url.slice(1, url.length);
+            if (url !== "") {
+                if (url !== currState.url) {
+                    var r = getRouter({
+                        url: url
+                    })
+                    if (r)
+                        go(r.name);
+                }
+            }
+        }, 60);
+    }
+
+    function getRouter(obj) {
+        var k = Object.keys(obj)[0];
         var r = routers.filter(function (e) {
-            return e.name === name;
+            return e[k] === obj[k];
         });
         if (r.length > 0)
             return r[0];
     }
 
-    function findModelPropIndicesOf(str) {
-        var openIndices = [];
-        var closeIndices = [];
-        for (var i = 0; i < str.length; i++) {
-            var val = i === str.length - 1 ? val = str[i] : str[i] + str[i + 1];
-            if (val === "{{")
-                openIndices.push(i);
-            else if (val === "}}")
-                closeIndices.push(i);
-        }
-        return {
-            openIndices: openIndices,
-            closeIndices: closeIndices
-        };
-    }
-
     function rend(str, scope) {
-        debugger;
         var bindingBoard = {};
         var elements = new DOMParser().parseFromString(str, "text/html").firstChild.childNodes[1].children;
-        var i = 0;
         do {
-            var prop;
-            var e = elements[i];
-            if (e.tagName === "INPUT" || e.tagName === "SELECT" || e.tagName === "TEXTAREA") {
-                if (e.hasAttribute('ng-model'))
-                    prop = e.getAttribute('ng-model');
-                if (!scope.hasOwnProperty(prop))
-                    throw "Error3";
-                if (!bindingBoard.hasOwnProperty(prop)) {
-                    bindingBoard[prop] = {};
-                    if (!bindingBoard[prop].input)
-                        bindingBoard[prop].input = [];
-                    bindingBoard[prop].input.push(e);
-                } else {
-                    if (!bindingBoard[prop].input)
-                        bindingBoard[prop].input = [];
-                    bindingBoard[prop].input.push(e);
-                }
-            } else {
-                if (e.hasAttribute('ng-bind')) {
-                    prop = e.getAttribute('ng-bind');
-                    if (!scope.hasOwnProperty(prop))
-                        throw "Error3";
-                    if (!bindingBoard.hasOwnProperty(prop)) {
-                        bindingBoard[prop] = {};
-                        if (!bindingBoard[prop].display)
-                            bindingBoard[prop].display = [];
-                        bindingBoard[prop].display.push(e);
-                    } else {
-                        if (!bindingBoard[prop].display)
-                            bindingBoard[prop].display = [];
-                        bindingBoard[prop].display.push(e);
-                    }
-                }
-            }
-            container[0].appendChild(e);
+            container[0].appendChild(elements[0]);
         } while (elements.length > 0)
+        var bindingElements = container[0].querySelectorAll("input[ng-model],textarea[ng-model],select[ng-model]");
+        var displayElements = container[0].querySelectorAll("[ng-bind]:not(input):not(textarea):not(select)");
 
-        for (k in bindingBoard) {
-            if (bindingBoard.hasOwnProperty(k)) {
-                if (bindingBoard[k].input)
-                    for (var i = 0; i < bindingBoard[k].input.length; i++) {
-                        var input = bindingBoard[k].input[i];
-                        input.addEventListener('input', function (eventArg) {
-                            input = eventArg.currentTarget;
-                            k = input.getAttribute('ng-model');
-                            scope[k] = input.value;
-                            for (var j = 0; j < bindingBoard[k].display.length; j++)
-                                bindingBoard[k].display[j].innerText = scope[k];
-                        }, false);
-                    }
-                if (bindingBoard[k].display)
-                    for (var j = 0; j < bindingBoard[k].display.length; j++)
-                        bindingBoard[k].display[j].innerText = scope[k];
-            }
+        for (var i = 0; i < bindingElements.length; i++) {
+            var path = bindingElements[i].getAttribute('ng-model');
+
+            bindingElements[i].addEventListener('input', function (e) {
+                input = e.currentTarget;
+                var path = input.getAttribute('ng-model');
+                objectDeepAccess(scope, path, input.value);
+                for (var j = 0; j < bindingBoard[path].display.length; j++)
+                    bindingBoard[path].display[j].innerText = objectDeepAccess(scope, path);
+            });
+
+            if (!bindingBoard.hasOwnProperty(path))
+                bindingBoard[path] = {
+                    display: [],
+                    input: []
+                };
+            bindingBoard[path].input.push(bindingElements[i]);
         }
+
+        for (var i = 0; i < displayElements.length; i++) {
+            var path = displayElements[i].getAttribute('ng-bind');
+            displayElements[i].innerText = objectDeepAccess(scope, path);
+            if (!bindingBoard.hasOwnProperty(path))
+                bindingBoard[path] = {
+                    display: [],
+                    input: []
+                };
+            bindingBoard[path].display.push(displayElements[i]);
+        }
+    }
+
+    function objectDeepAccess(obj, path, value) {
+        var _obj = obj;
+        var pList = path.split('.');
+        var len = pList.length - 1;
+        for (var i = 0; i < len; i++) {
+            var e = pList[i];
+            if (!_obj.hasOwnProperty(e)) _obj[e] = {};
+            _obj = _obj[e];
+        }
+        if (value !== undefined)
+            _obj[pList[len]] = value;
+        else
+            return _obj[pList[len]];
     }
 
     function go(name) {
-        var r = getRouter(name);
-        var scope = {};
-        container[0].innerHTML = "";
-        // var oldScope = window.$scope;
-        // window.$scope = scope;
-        r.controller(scope);
-        // scope = window.$scope;
-        // window.$scope = oldScope;
-        rend(r.template, scope);
-        // container[0].innerHTML = renderTemplate(r.template, scope);
-        window.location.hash = r.url;
-        currState = r;
-    }
-
-    function replaceModel(str, openIndices, closeIndices, scope) {
-        var result = "";
-        for (var i = 0; i < openIndices.length; i++) {
-            var open = openIndices[i];
-            var close = closeIndices[i];
-            var prop = str.substring(open + 2, close);
-            if (!scope.hasOwnProperty(prop))
-                throw "Error2";
-            if (i === 0)
-                result += str.substring(0, open) + scope[prop];
-            else
-                result += str.substring(closeIndices[i - 1] + 2, open) + scope[prop];
+        var execute = true,
+            nextState = getRouter({
+                name: name
+            }),
+            prevState = getRouter({
+                url: window.location.hash
+            }),
+            eObj = {
+                preventDefault: function () {
+                    execute = false;
+                }
+            },
+            scope = {};
+        events["onPreStateChanged"](eObj, prevState, nextState);
+        if (execute) {
+            container[0].innerHTML = "";
+            if (typeof nextState.controller === 'function') {
+                nextState.controller(scope);
+                loadHtml(prevState, nextState, scope, eObj);
+            } else if (typeof nextState.controller === 'string') {
+                loadController(nextState.controller, function () {
+                    currController(scope);
+                    loadHtml(prevState, nextState, scope, eObj);
+                });
+            }
         }
-        result += str.substring(closeIndices[closeIndices.length - 1] + 2);
-        return result;
     }
 
-    function renderTemplate(str, scope) {
-        str = str.replace(/\s+/g, ' ');
-        var indices = findModelPropIndicesOf(str);
-        if (indices.openIndices.length !== indices.closeIndices.length)
-            throw "Error1";
-        var result = replaceModel(str, indices.openIndices, indices.closeIndices, scope);
-        return result;
+    function loadHtml(prevState, nextState, scope, eObj) {
+        if (nextState.template) {
+            rend(nextState.template, scope);
+            changeURL(prevState, nextState);
+            events["onStateChanged"](eObj, currState);
+        } else if (nextState.templatePath) {
+            loadFile(nextState.templatePath, function (content) {
+                rend(content, scope);
+                changeURL(prevState, nextState);
+                events["onStateChanged"](eObj, nextState);
+            });
+        }
+    }
+
+    function loadFile(theUrl, callback) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+                callback(xmlHttp.responseText);
+        }
+        xmlHttp.open("GET", theUrl, true);
+        xmlHttp.send(null);
+    }
+
+    function loadController(src, callback) {
+        var script,
+            tag,
+            r = false;
+
+        var oldFiles = document.querySelector('script[curr-controller]');
+        if (oldFiles) oldFiles.remove();
+
+        script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = src.indexOf(".js") !== src.length - 3 ? src + ".js" : src;
+        script.setAttribute('curr-controller', '');
+        script.onload = script.onreadystatechange = function () {
+            if (!r && (!this.readyState || this.readyState == 'complete')) {
+                r = true;
+                callback();
+            }
+        };
+        tag = document.getElementsByTagName('script')[0];
+        tag.parentNode.insertBefore(script, tag);
+    }
+
+    function changeURL(prevState, nextState) {
+        preState = prevState;
+        window.location.hash = nextState.url;
+        currState = nextState;
     }
 
     return {
         register: function (r) {
+            debugger;
             register(r);
+            return this;
         },
         go: function (name) {
             go(name);
         },
         reload: function () {
             go(currState.name);
+        },
+        onPreStateChanged: function (callback) {
+            return events["onPreStateChanged"] = callback;
+        },
+        onStateChanged: function (callback) {
+            events["onStateChanged"] = callback;
+        },
+        on: function (eventName, callback) {
+            eventName = "on" + eventName.charAt(0).toUpperCase() + eventName.substring(1);
+            events[eventName] = callback;
+        },
+        Controller: function (controller) {
+            currController = controller;
         }
     }
 }())
